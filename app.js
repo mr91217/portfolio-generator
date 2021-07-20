b309f71228a4af3350b1abd7fbd8a9b008d139ee
ChunYu Chang <mr91217@gmail.com>
2021-07-19 21:41:18 -0400

    add promise

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+// const fs = require('fs');
 const generatePage = require('./src/page-template');
+// const generateSite = require('./utils/generate-site.js');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+
 
 // const pageHTML = generatePage(name, github);
 
@@ -162,17 +165,47 @@ Add a New Project
   
 //   promptUser().then(answers => console.log(answers));
 
-promptUser()
-.then(promptProject)
-.then(portfolioData => {
+// promptUser()
+// .then(promptProject)
+// .then(portfolioData => {
 
-    // will be uncommented in lesson 4
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-      console.log('Page created! Check out index.html in this directory to see it!');
+//     // will be uncommented in lesson 4
+//     const pageHTML = generatePage(portfolioData);
+//     fs.writeFile('./dist/index.html', pageHTML, err => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+//         console.log('Page created! Check out index.html in this directory to see it!');
+      
+//         fs.copyFile('./src/style.css', './dist/style.css', err => {
+//           if (err) {
+//             console.log(err);
+//             return;
+//           }
+//           console.log('Style sheet copied successfully!');
+//         });
+//       });
+// });
+
+    promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
-});
 
 // {
 //     name: 'Lernantino',
